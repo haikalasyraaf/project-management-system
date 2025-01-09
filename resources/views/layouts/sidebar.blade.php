82f6194ecf273193b6f445ed4f5d343d5b1c0092
Haikal Asyraaf <acapkhalik@gmail.com>
2025-01-10 03:30:46 +0800

    active sidebar on multiple route

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,15 +3,15 @@
         <i class="sidebar-icon" data-feather="home"></i>
         <span class="ms-3 d-none d-sm-inline">Dashboard</span>
     </a>
-    <a href="{{ route('project.index') }}" class="nav-link align-middle {{ request()->routeIs('project.index') ? 'active' : '' }}">
+    <a href="{{ route('project.index') }}" class="nav-link align-middle {{ request()->routeIs('project.index', 'project.create', 'project.edit', 'expense.index', 'expense.create', 'expense.edit') ? 'active' : '' }}">
         <i class="sidebar-icon" data-feather="server"></i>
         <span class="ms-3 d-none d-sm-inline">Project</span>
     </a>
-    <a href="{{ route('invoice.index') }}" class="nav-link align-middle {{ request()->routeIs('invoice.index') ? 'active' : '' }}">
+    <a href="{{ route('invoice.index') }}" class="nav-link align-middle {{ request()->routeIs('invoice.index', 'invoice.create', 'invoice.show') ? 'active' : '' }}">
         <i class="sidebar-icon" data-feather="clipboard"></i>
         <span class="ms-3 d-none d-sm-inline">Invoice</span>
     </a>
-    <a href="{{ route('user.index') }}" class="nav-link align-middle {{ request()->routeIs('user.index') ? 'active' : '' }}">
+    <a href="{{ route('user.index') }}" class="nav-link align-middle {{ request()->routeIs('user.index', 'user.create', 'user.edit') ? 'active' : '' }}">
         <i class="sidebar-icon" data-feather="users"></i>
         <span class="ms-3 d-none d-sm-inline">User</span>
     </a>
