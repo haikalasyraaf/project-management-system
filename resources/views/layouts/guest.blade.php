5049d78f597cfcbd64de817682a1a640afe38513
Haikal Asyraaf <acapkhalik@gmail.com>
2025-01-10 14:54:21 +0800

    refactor login page design

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,16 +14,18 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+    <body>
+        <div class="d-flex justify-content-center align-items-center min-vh-100">
+            <div class="w-100" style="max-width: 400px;">
+                <div class="card rounded-4 shadow-lg" style="background-color: rgba(255, 255, 255, 0.2)">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <a href="/"><x-application-logo style="width: 120px; height: 120px"/></a>                            
+                        </div>
+                        <br>
+                        {{ $slot }}
+                    </div>
+                </div>
             </div>
         </div>
     </body>
